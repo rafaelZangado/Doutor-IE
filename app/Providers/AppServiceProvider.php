a75200ee77144c712e3189aa8797e54dddf20dd1
rafaelZangado <fael.andrade0@gmail.com>
2024-06-29 18:16:56 -0300

    :rocket: Projeto Backend php-API REST

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\LivrosRepository;
+use App\Repositories\LivrosRepositoryInterface;
+use App\Services\LivrosServices;
+use App\Repositories\IndicesRepository;
+use App\Repositories\IndicesRepositoryInterface;
+use App\Services\IndicesServices;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +19,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(LivrosRepositoryInterface::class, LivrosRepository::class);
+        $this->app->singleton(LivrosServices::class, function ($app) {
+            return new LivrosServices($app->make(LivrosRepositoryInterface::class));
+        });
+
+        $this->app->singleton(IndicesRepositoryInterface::class, IndicesRepository::class);
+        $this->app->singleton(IndicesServices::class, function ($app) {
+            return new IndicesServices($app->make(IndicesRepositoryInterface::class));
+        });
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndiceController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LivroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('v1/auth/token', [AuthController::class, 'getToken']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('v1/livros', [LivroController::class, 'index']);
+    Route::post('v1/livros', [LivroController::class, 'store']);
+    Route::put('v1/livros/{livro}', [LivroController::class, 'update']);
+    Route::delete('v1/livros/{livro}', [LivroController::class, 'destroy']);
+    Route::post('v1/indices', [IndiceController::class, 'store']);
+    Route::post('v1/livros/{livroId}/importar-indices-xml', [LivroController::class, 'importarIndicesXML']);
+});
+
+
